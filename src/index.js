63a82cb10251cd321d7374306c0a894874688f84
jodusnodus <thomas842@hotmail.com>
2016-07-30 20:09:37 +0200

    Add facing mode selector

--- src/index.js
+++ src/index.js
@@ -8,8 +8,13 @@ class Demo extends Component {
     super(props)
     this.state = {
       result: 'No result',
+      facingMode: 'front',
       error: false,
     }
+
+    this.handleFacingModeChange = this.handleFacingModeChange.bind(this)
+    this.handleScan = this.handleScan.bind(this)
+    this.handleError = this.handleError.bind(this)
   }
   handleScan(data){
     this.setState({
@@ -20,21 +25,43 @@ class Demo extends Component {
     this.setState({
       error: true,
     })
-    console.error('error')
+  }
+  handleFacingModeChange(e){
+    this.setState({
+      facingMode: e.target.value,
+    })
   }
   render(){
     if(this.state.error){
       return h('h3', 'Demo could not be displayed.')
     }else{
+      console.log(this.refs.reader)
       return h('div', [
         h(QrReader, {
-          handleScan: this.handleScan.bind(this),
-          handleError: this.handleError.bind(this)
+          handleScan: this.handleScan,
+          handleError: this.handleError,
+          facingMode: this.state.facingMode,
+          ref: 'reader',
         }),
-        h('h3', `Decoded QR-Code: ${this.state.result}`)
+        h('p', [
+          'Facing Mode: ',
+          h('select', {
+            onChange: this.handleFacingModeChange,
+            style: {
+              marginTop: 10,
+            },
+          }, [
+            h('option', {value: 'front'}, 'Front'),
+            h('option', {value: 'back'}, 'Back'),
+          ]),
+        ]),
+        h('h3', `Decoded QR-Code: ${this.state.result}`),
       ])
     }
   }
 }
 
-ReactDOM.render(h(Demo), document.getElementById('demo-container'))
+ReactDOM.render(
+  h(Demo),
+  document.getElementById('demo-container')
+)
