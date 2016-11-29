3120f214884232c4674e708213fc3b6046dd1a59
jodusnodus <thomas842@hotmail.com>
2016-11-29 14:31:54 +0100

    Clean up html & upgrade react-qr-reader

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
-import { Component } from 'react'
+import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
-import h from 'react-hyperscript'
-import QrReader from 'react-qr-reader'
+import QrReader from './react-qr-reader'
 
 class Demo extends Component {
   constructor(props){
@@ -9,10 +8,14 @@ class Demo extends Component {
     this.state = {
       result: 'No result',
       error: false,
+      facingMode: 'front',
+      interval: 500,
     }
 
     this.handleScan = this.handleScan.bind(this)
     this.handleError = this.handleError.bind(this)
+    this.handleFacingModeChange = this.handleFacingModeChange.bind(this)
+    this.handleIntervalChange = this.handleIntervalChange.bind(this)
   }
   handleScan(data){
     this.setState({
@@ -24,27 +27,62 @@ class Demo extends Component {
       error: true,
     })
   }
+  handleFacingModeChange(e){
+    this.setState({
+      facingMode: e.target.value,
+    })
+  }
+  handleIntervalChange(e){
+    this.setState({
+      interval: parseInt(e.target.value),
+    })
+  }
   render(){
     if(this.state.error){
-      return h('h3', 'Demo could not be displayed.')
+      return <h3>Demo could not be displayed.</h3>
     }else{
-      return h('div', [
-        h(QrReader, {
-          handleScan: this.handleScan,
-          handleError: this.handleError,
-          interval: 500,
-          ref: 'reader',
-          previewStyle: {
-            width: '100%',
-          },
-        }),
-        h('h3', `Decoded QR-Code: ${this.state.result}`),
-      ])
+      return (
+        <div>
+          <div
+            style={{
+              margin: '10px',
+              display: 'flex',
+              justifyContent: 'space-between',
+            }}>
+            <select
+              value={this.state.facingMode}
+              onChange={this.handleFacingModeChange}>
+              <option value="front">Front Camera</option>
+              <option value="rear">Rear Camera</option>
+            </select>
+            <span>
+              <label htmlFor="interval">Interval in ms: </label>
+              <input
+                id="interval"
+                type="number"
+                value={this.state.interval}
+                onChange={this.handleIntervalChange}
+                />
+            </span>
+          </div>
+          <QrReader
+            handleScan={this.handleScan}
+            handleError={this.handleError}
+            facingMode={this.state.facingMode}
+            interval={this.state.interval}
+            ref="reader"
+            previewStyle={{
+              width: '100%',
+            }}
+            />
+          <h3>Decoded QR-Code: {this.state.result}</h3>
+        </div>
+      )
     }
   }
 }
 
 ReactDOM.render(
-  h(Demo),
+  <Demo/>,
   document.getElementById('demo-container')
 )
