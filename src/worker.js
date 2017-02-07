2506b831c1963fc861f8b2c4d2f4405054e0fb05
jodusnodus <billiet.thomas@outlook.com>
2017-02-07 19:51:51 +0100

    Fix trailing comma

--- src/worker.js
+++ src/worker.js
@@ -4,7 +4,7 @@ self.addEventListener('message', function(e) {
   var decoded = jsQR.decodeQRFromImage(
     e.data.data,
     e.data.width,
-    e.data.height,
+    e.data.height
   )
   postMessage(decoded)
 })
