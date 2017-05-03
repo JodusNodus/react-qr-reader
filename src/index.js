3d92d803b5c72ae2d9f113f995c842b02d95ca01
jodusnodus <billiet.thomas@outlook.com>
2017-05-03 13:54:07 +0200

    Upgrade react-qr-reader & Improve
    - Use legacyMode as fallback
    - Improved styling
    - use onLoad

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ class Demo extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: true,
+      legacyMode: false,
       result: 'No result',
       error: false,
       facingMode: 'front',
@@ -14,14 +16,22 @@ class Demo extends Component {
 
     this.handleScan = this.handleScan.bind(this);
     this.handleError = this.handleError.bind(this);
+    this.handleLoad = this.handleLoad.bind(this);
     this.handleFacingModeChange = this.handleFacingModeChange.bind(this);
     this.handleDelayChange = this.handleDelayChange.bind(this);
+    this.handleImgSubmit = this.handleImgSubmit.bind(this);
   }
-  handleScan(data) {
-    this.setState({ result: data });
+  handleScan(result) {
+    if(result){
+      this.setState({ result });
+    }
   }
-  handleError() {
-    this.setState({ error: true });
+  handleError(err) {
+    this.setState({ legacyMode: true })
+  }
+  handleLoad() {
+    console.log("hello world")
+    this.setState({ loading: false });
   }
   handleFacingModeChange(e) {
     this.setState({ facingMode: e.target.value });
@@ -29,45 +39,74 @@ class Demo extends Component {
   handleDelayChange(e) {
     this.setState({ delay: parseInt(e.target.value) });
   }
+  handleImgSubmit(){
+    this.refs.reader.openImageDialog()
+  }
   render() {
+    const centerContainerStyle = {
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: 100,
+    }
+    const hozContainerStyle = {
+      margin: "10px",
+      display: "flex",
+      justifyContent: "space-between",
+    };
+
     if (this.state.error) {
-      return <h3>Demo could not be displayed.</h3>;
+      return (
+        <div style={centerContainerStyle}>
+          <h3>Error</h3>
+        </div>
+      )
     } else {
       return (
         <div>
-          <div
-            style={{
-              margin: '10px',
-              display: 'flex',
-              justifyContent: 'space-between',
-            }}
-          >
-            <select
-              value={this.state.facingMode}
-              onChange={this.handleFacingModeChange}
-            >
-              <option value="front">Front Camera</option>
-              <option value="rear">Rear Camera</option>
-            </select>
-            <span>
-              <label htmlFor="delay">Delay in ms: </label>
-              <input
-                id="delay"
-                type="number"
-                value={this.state.delay}
-                onChange={this.handleDelayChange}
+          {this.state.loading && (
+            <div style={centerContainerStyle}>
+              <h3>Loading</h3>
+            </div>
+          )}
+          <div style={{display: this.state.loading ? 'none' : 'block'}}>
+            {!this.state.legacyMode ? (
+              <div style={hozContainerStyle}>
+                <select
+                  value={this.state.facingMode}
+                  onChange={this.handleFacingModeChange}
+                  >
+                  <option value="front">Front Camera</option>
+                  <option value="rear">Rear Camera</option>
+                </select>
+                <span>
+                  <label htmlFor="delay">Delay in ms: </label>
+                  <input
+                    id="delay"
+                    type="number"
+                    value={this.state.delay}
+                    onChange={this.handleDelayChange}
+                    />
+                </span>
+              </div>
+            ) : (
+              <div style={hozContainerStyle}>
+                <h3>Webcam not supported</h3>
+                <button onClick={this.handleImgSubmit}>Submit an Image</button>
+              </div>
+            )}
+            <QrReader
+              onScan={this.handleScan}
+              onError={this.handleError}
+              onLoad={this.handleLoad}
+              facingMode={this.state.facingMode}
+              delay={this.state.delay}
+              legacyMode={this.state.legacyMode}
+              ref="reader"
+              style={{ width: '100%', maxWidth: 700, margin: 'auto' }}
               />
-            </span>
+            <h3>Decoded QR-Code: {this.state.result}</h3>
           </div>
-          <QrReader
-            onScan={this.handleScan}
-            onError={this.handleError}
-            facingMode={this.state.facingMode}
-            delay={this.state.delay}
-            ref="reader"
-            style={{ width: '100%' }}
-          />
-          <h3>Decoded QR-Code: {this.state.result}</h3>
         </div>
       );
     }
