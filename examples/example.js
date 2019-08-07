000376a2f56b3dd4b055a69378f009bca2ad96df
Tuomas Aho <tuomasaho91@gmail.com>
2019-08-07 23:44:41 +0300

    Update examples

--- examples/example.js
+++ examples/example.js
@@ -1,39 +1,112 @@
-import React, { Component } from 'react'
+import { Component } from 'react'
 import QrReader from 'react-qr-reader2'
+import Slider, { createSliderWithTooltip } from 'rc-slider'
+import 'rc-slider/assets/index.css'
+const SliderWithTooltip = createSliderWithTooltip(Slider)
 
-class Example extends Component {
-  constructor(props){
+function timeout (ms) {
+  return new Promise(resolve => setTimeout(resolve, ms))
+}
+
+export default class QrCodeReader extends Component {
+  constructor (props) {
     super(props)
     this.state = {
-      delay: 500,
-      result: 'No result',
+      result: '',
+      delay: 1000,
+      hasZoomCapabilities: false,
+      zoomCapabilities: { min: 1, max: 1, step: 1 },
+      constraints: {
+        advanced: [{ zoom: 1 }],
+      },
     }
-
     this.handleScan = this.handleScan.bind(this)
+    this.onChangeZoom = this.onChangeZoom.bind(this)
+    this.onLoadQRCodeScanner = this.onLoadQRCodeScanner.bind(this)
   }
-  handleScan(result){
-    if(result){
-      this.setState({ result })
+
+  async onLoadQRCodeScanner (args) {
+    if (!args) {
+      return
+    }
+    const { streamTrack } = args
+    // https://bugs.chromium.org/p/chromium/issues/detail?id=711524
+    await timeout(1000)
+    const capabilities = streamTrack.getCapabilities()
+    if ('zoom' in capabilities) {
+      const { min, max, step } = capabilities.zoom
+      this.setState({
+        streamTrack,
+        hasZoomCapabilities: true,
+        zoomCapabilities: {
+          min,
+          max,
+          step,
+        },
+        constraints: {
+          advanced: [{ zoom: min }],
+        },
+      })
     }
   }
-  handleError(err){
-    console.error(err)
+
+  handleScan (data) {
+    if (data) {
+      this.setState({ result: data })
+    }
   }
-  render(){
+
+  handleError (err) {
+    console.error(err) // eslint-disable-line no-console
+  }
+
+  render () {
+    const { delay,
+      zoomCapabilities,
+      hasZoomCapabilities,
+      constraints,
+      result
+    } = this.state
+    const { min, max, step } = zoomCapabilities
     const previewStyle = {
       height: 240,
       width: 320,
     }
 
-    return(
+    return (
       <div>
         <QrReader
-          delay={this.state.delay}
-          style={previewStyle}
+          delay={delay}
           onError={this.handleError}
           onScan={this.handleScan}
-          />
-        <p>{this.state.result}</p>
+          onLoad={this.onLoadQRCodeScanner}
+          style={{ width: '100%', ...previewStyle }}
+        />
+        {hasZoomCapabilities && (
+          <div>
+            <div>
+              zoom {constraints.advanced[0].zoom}, min: {min}, max:{' '}
+              {max}, step: {step}
+            </div>
+            <SliderWithTooltip
+              onChange={this.onChangeZoom}
+              min={min}
+              max={max}
+              step={step}
+              value={constraints.advanced[0].zoom}
+              trackStyle={{ backgroundColor: 'blue', height: 10 }}
+              handleStyle={{
+                borderColor: 'blue',
+                height: 28,
+                width: 28,
+                marginLeft: -14,
+                marginTop: -9,
+                backgroundColor: 'black',
+              }}
+            />
+          </div>
+        )}
+        <p>{result}</p>
       </div>
     )
   }
