af360f9d201d0c8931b540c7023ef4af5297e063
memenashi <shunsuke.vrml@icloud.com>
2023-05-02 22:44:24 +0900

    update rollup and dependencies

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,7 +6,7 @@ import external from 'rollup-plugin-peer-deps-external';
 import dts from 'rollup-plugin-dts';
 import webWorkerLoader from 'rollup-plugin-web-worker-loader';
 
-const packageJson = require('./package.json');
+import packageJson from './package.json' assert { type: 'json' };
 const extensions = ['.js', '.jsx', '.ts', '.tsx'];
 
 export default [
